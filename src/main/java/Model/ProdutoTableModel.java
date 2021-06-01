09552e30960a78b45925324a78ec4a3715f357d4
felip <felip@DESKTOP-6ASM7M6>
2021-06-01 17:11:46 -0300

    Feito ajustes e o programa esta certo e completo

--- src/main/java/Model/ProdutoTableModel.java
+++ src/main/java/Model/ProdutoTableModel.java
@@ -72,4 +72,7 @@ public class ProdutoTableModel extends AbstractTableModel {
         this.dados.remove(linha);
         this.fireTableRowsDeleted(linha, linha);
     }
+    public Produto pegaDadosLinha(int linha) {
+        return dados.get(linha);
+    }
 }
